77e2c21979fd30711a1392816c52a7a1fd301bd0
SirWinstonFR <miniprodgaming@gmail.com>
2026-06-01 21:20:25 +0200

    Update grande_societe.js

--- grande_societe.js
+++ grande_societe.js
@@ -11,7 +11,7 @@ const GS_CFG = {
   SHEET_ID: '1L9hbQuAD9A4WQFG1G47teZlUPM6-JkMmuuX2Ys-TYt8',
   APPS_SCRIPT: 'https://script.google.com/macros/s/AKfycbyCaQI2c5ds2uCmoeCw6_fALjh-8ii05fkOVgZmWPhbY64vyYbrNcFvqbFKRb7rUwyxwQ/exec',
   GIDS: {
-    Gouvernement: '', // À renseigner
+    Gouvernement: '789610864', // À renseigner
     nasa:         '',
     cia:          '',
     entreprises:  '',
